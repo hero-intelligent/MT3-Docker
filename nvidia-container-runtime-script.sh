b08ff272b475e493af12b91fde7b00f72be601e9
hero-intelligent <112150507+hero-intelligent@users.noreply.github.com>
2023-03-31 10:13:58 +0800

    Update nvidia-container-runtime-script.sh

--- nvidia-container-runtime-script.sh
+++ nvidia-container-runtime-script.sh
@@ -1,16 +1,17 @@
-# # nvidia-container-runtime-script.sh
+# nvidia-container-runtime-script.sh
 
-# sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
-#   sudo apt-key add -
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-# sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
-#   sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-# sudo apt-get update
-
-
+sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
 sudo systemctl restart docker
+
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+# sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+# sudo systemctl restart docker
